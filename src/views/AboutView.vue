75cde1548523c6c519bdbb840a5892e926f830c4
agoyeeee <prayogaputra70@gmail.com>
2024-11-02 19:08:49 +0700

    update

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,9 +1,30 @@
-<script setup lang="ts">
-import MainLayout from '@/layouts/MainLayout.vue'
-</script>
-
 <template>
   <MainLayout>
-    <h1>About view</h1>
+    <HeroSection>
+    </HeroSection>
+    <StaticsPanel>
+    </StaticsPanel>
+    <MissionVision>
+    </MissionVision>
+    <CreativeTeam/>
+    <BlogSection>
+    </BlogSection>
+    <TeamGrid>
+    </TeamGrid>
   </MainLayout>
 </template>
+
+<script setup lang="ts">
+import MainLayout from '@/layouts/MainLayout.vue'
+import HeroSection from '@/components/AboutUs/HeroSection.vue'
+import StaticsPanel from '@/components/AboutUs/StaticsPanel.vue'
+import MissionVision from '@/components/AboutUs/MissionVision.vue'
+import BlogSection from '@/components/AboutUs/BlogSection.vue'
+import TeamGrid from '@/components/AboutUs/TeamGrid.vue'
+import CreativeTeam from '@/components/AboutUs/CreativeTeam.vue'
+
+</script>
+
+<style scoped>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,24 @@
-<script setup lang="ts">
-import MainLayout from '@/layouts/MainLayout.vue'
-</script>
-
 <template>
   <MainLayout>
-    <h1>Homeview</h1>
+    <HeroSection />
+    <FeaturedPosts />
+    <AboutSection />
+    <CategorySection />
+    <StorySection />
+    <AuthorGrid />
+    <LogoSection />
+    <TestimonialSection />
   </MainLayout>
 </template>
+
+<script setup lang="ts">
+import MainLayout from '@/layouts/MainLayout.vue';
+import HeroSection from '@/components/Home/HeroSection.vue';
+import FeaturedPosts from '@/components/Home/FeaturedPost.vue';
+import AboutSection from '@/components/Home/AboutSection.vue';
+import CategorySection from '@/components/Home/CategorySection.vue';
+import StorySection from '@/components/Home/StorySection.vue';
+import AuthorGrid from '@/components/Home/AuthorGrid.vue';
+import LogoSection from '@/components/Home/LogoSection.vue';
+import TestimonialSection from '@/components/Home/TestimonialSection.vue';
+</script>
